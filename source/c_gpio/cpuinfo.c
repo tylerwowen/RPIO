9f2ca9cae5e4088679a17e56038970d0613458ee
Weimin Ouyang <tylerwowen@users.noreply.github.com>
2015-03-24 18:24:01 -0700

    Added support for Pi 2
     BCM2708 for Pi 1, BCM2709 for Pi 2

--- source/c_gpio/cpuinfo.c
+++ source/c_gpio/cpuinfo.c
@@ -49,7 +49,8 @@ get_cpuinfo_revision(char *revision_hex)
     while(!feof(fp)) {
         fgets(buffer, sizeof(buffer) , fp);
         sscanf(buffer, "Hardware	: %s", hardware);
-        if (strcmp(hardware, "BCM2708") == 0)
+        // BCM2708 for Pi 1, BCM2709 for Pi 2
+        if (strcmp(hardware, "BCM2708") == 0 || strcmp(hardware, "BCM2709") == 0)
             rpi_found = 1;
         sscanf(buffer, "Revision	: %s", revision_hex);
     }
